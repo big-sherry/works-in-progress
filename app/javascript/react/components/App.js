3516d42d07a5cac810ab1bef18e8653bcfa3fcc4
Jamie Sheridan <jpsheridan95@gmail.com>
2022-11-15 16:04:41 -0500

    Moved Prompt Component Files into Folders

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
-import PromptsIndexContainer from './Prompts/PromptsIndexContainer'
+import PromptsIndexContainer from './Prompts/Index/PromptsIndexContainer'
 import FullPage from './FullPage'
 
 const App = (props) => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' render={() => 
+        <Route exact path='/'>
           <FullPage 
             page={<PromptsIndexContainer />}
             user={<h1>Wow</h1>}
-          />}/>
+          />
+        </Route>
         <Route exact path='/' component={PromptsIndexContainer} />
       </Switch>
     </BrowserRouter>
