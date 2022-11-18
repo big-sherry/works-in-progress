afd1fdeb360df8104c7cc53778d60c3a63124fea
Jamie Sheridan <jpsheridan95@gmail.com>
2022-11-18 15:48:22 -0500

    Form Created and Styled (Basic)

--- app/javascript/react/components/Responses/New/NewResponseForm.js
+++ app/javascript/react/components/Responses/New/NewResponseForm.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewResponseForm = (props) => {
+    const [newResponse, setNewResponse] = useState({ body: "" })
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("hello")
+    }
+
+    const handleFormChange = (event) => {
+        const updatingField = event.currentTarget.name
+        setNewResponse({
+            ...newResponse,
+            [updatingField]: event.currentTarget.value
+        })
+    }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label
-            </form>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div className="new-response">
+                <label>
+                    <div className="response-label">
+                        Your Response:
+                    </div>
+                    <div className="new-response-textarea">
+                        <textarea 
+                            name="body"
+                            id="body"
+                            value={newResponse.body}
+                            onChange={handleFormChange}
+                        />
+                    </div>
+                </label>
+                <div className="submit-button">
+                    <input type="submit" value="Add Response"/>
+                </div>
+            </div>
+        </form>
     )
 }
 
