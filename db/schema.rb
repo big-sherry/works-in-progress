77341ed556e1ed6e5d3658c4b48f529b7e02e3af
Jamie Sheridan <jpsheridan95@gmail.com>
2022-11-21 11:47:04 -0500

    Added AWS Connection (also fixing issues on prompt show)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_11_175809) do
+ActiveRecord::Schema.define(version: 2022_11_19_190519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2022_11_11_175809) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_pic", default: "app/assets/images/bc75882d906b263fbe0550fe59dc7b21.jpeg"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
