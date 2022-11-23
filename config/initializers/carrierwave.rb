9168b88a6b428ffd2187ae0bdaa1dbfb6f2167ce
Jamie Sheridan <jpsheridan95@gmail.com>
2022-11-23 10:06:58 -0500

    Users Can Upload Profile Pic

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
         if Rails.env.production?
             config.fog_directory  = ENV["PRODUCTION_S3_BUCKET"]
         else
-            config.fog_directory  = ENV["DEVELOPMENT_S3_BUCKET"]
+            config.fog_directory  = ENV["PRODUCTION_S3_BUCKET"]
         end
     end
 end
